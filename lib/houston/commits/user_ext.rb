bb69448cccc4ece1f9bbb0e89b2648039481a9fc
Bob Lail <bob.lailfamily@gmail.com>
2017-01-17 10:12:00 -0600

    [fix] Fixed extending User with `find_by_github_username` (2m)

--- lib/houston/commits/user_ext.rb
+++ lib/houston/commits/user_ext.rb
@@ -12,18 +12,20 @@ module Houston
       # Extract to Houston::GitHub
       # ------------------------------------------------------------------------- #
 
-      def self.find_by_github_username(username)
-        find_by_prop "github.username", username do |username|
+      module ClassMethods
+        def find_by_github_username(username)
+          find_by_prop "github.username", username do |username|
 
-          # Look up the email address of the GitHub user and see if we can
-          # identify the Houston user by the GitHub user's email address.
-          user = Houston.github.user(username)
-          user = find_by_email_address user.email if user
+            # Look up the email address of the GitHub user and see if we can
+            # identify the Houston user by the GitHub user's email address.
+            user = Houston.github.user(username)
+            user = find_by_email_address user.email if user
 
-          # We couldn't find the user by their email address, now
-          # we'll look at their nicknames
-          user = find_by_nickname username unless user
+            # We couldn't find the user by their email address, now
+            # we'll look at their nicknames
+            user = find_by_nickname username unless user
 
+          end
         end
       end
 
