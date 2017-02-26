d2be44d19fb1208628b4e5575aa943021f100639
Bob Lail <bob.lailfamily@gmail.com>
2017-02-26 13:08:14 -0600

    [skip] Added secret_key_base to dummy houston (1m)

--- test/dummy/houston.rb
+++ test/dummy/houston.rb
@@ -10,6 +10,7 @@ Houston.config do
 
   # Give dummy values to these required fields.
   host "houston.test.com"
+  secret_key_base "d8adaa0bc2ef682fe436ab22a4b2c0"
   mailer_sender "houston@test.com"
 
   # Mount this module on the dummy Houston application.
